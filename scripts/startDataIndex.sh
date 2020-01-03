8bfa469986e74806c741d314d5df6a35ed44f440
Wojciech Trocki <wtrocki@redhat.com>
2020-01-03 16:03:02 +0000

    fix: Added folder checks to script

--- scripts/startDataIndex.sh
+++ scripts/startDataIndex.sh
@@ -1,8 +1,13 @@
 
 echo "Script requires your maven project to be compiled"
 
-
 DATA_INDEX_VERSION=0.4.0
-wget http://repo2.maven.org/maven2/org/kie/kogito/data-index-service/${DATA_INDEX_VERSION}/data-index-service-${DATA_INDEX_VERSION}-runner.jar
-java -jar  -Dkogito.protobuf.folder=`pwd`/target/classes/persistence data-index-service-${DATA_INDEX_VERSION}-runner.jar
+PERSISTENCE_FOLDER=target/classes/persistence
+
+
+[ ! -d ${PERSISTENCE_FOLDER} ] && echo "Persistence folder is missing. Make sure that your project was compiled" && exit 0
+
+wget -nc http://repo2.maven.org/maven2/org/kie/kogito/data-index-service/${DATA_INDEX_VERSION}/data-index-service-${DATA_INDEX_VERSION}-runner.jar
+cp -rf target/classes/persistence persistence
+java -jar  -Dkogito.protobuf.folder=`pwd`/persistence data-index-service-${DATA_INDEX_VERSION}-runner.jar
 
